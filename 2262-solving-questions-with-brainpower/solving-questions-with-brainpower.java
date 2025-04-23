189571cdd18554e57a31230b5cf705b34c995748
K Jaswanth <118440654+547Jaswanth@users.noreply.github.com>
2025-04-23 17:01:45 +0530

    Time: 10 ms (39.72%) | Memory: 108.6 MB (46.84%) - LeetSync

--- 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.java
+++ 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.java
@@ -3,18 +3,22 @@ class Solution {
         int n = questions.length;
         long[] dp = new long[n + 1];
 
-        for (int i = n - 1; i >= 0; i--) {
+        for (int i = 0; i < n; i++) {
+            dp[i] = Math.max(dp[i], i > 0 ? dp[i - 1] : 0);
+
             int points = questions[i][0];
             int brainpower = questions[i][1];
             int next = i + brainpower + 1;
 
-            long solve = points + (next < n ? dp[next] : 0);
-            long skip = dp[i + 1];
+            if (next < n) {
+                dp[next] = Math.max(dp[next], dp[i] + points);
+            } else {
+                dp[n] = Math.max(dp[n], dp[i] + points); // fix here
+            }
 
-            dp[i] = Math.max(solve, skip);
+            dp[i + 1] = Math.max(dp[i + 1], dp[i]);
         }
 
-        return dp[0];
-        
+        return dp[n];
     }
 }
