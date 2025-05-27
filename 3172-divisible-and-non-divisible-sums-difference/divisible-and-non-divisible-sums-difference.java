c9201e1172d23c5421c910300a17bc21cfc1284c
K Jaswanth <118440654+547Jaswanth@users.noreply.github.com>
2025-05-27 11:19:03 +0530

    Time: 1 ms (74.69%) | Memory: 41.2 MB (19.43%) - LeetSync

--- 3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.java
+++ 3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.java
@@ -3,17 +3,6 @@ class Solution {
         int non_div_sum=0;
         int div_sum=0;
         int i=1;
-        // for(int i=1;i<=n;i++)
-        // {
-        //     if(i%m==0){
-        //         div_sum+=i;
-        //     }
-        //     else{
-        //     non_div_sum+=i;
-        //     }
-        // }
-        // return non_div_sum - div_sum;
-
         while(i<=n)
         {
             if(i%m==0){
