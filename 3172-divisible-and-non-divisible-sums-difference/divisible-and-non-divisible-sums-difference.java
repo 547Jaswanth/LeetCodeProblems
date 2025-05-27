33d35baccde19e0c7ccb47e043a10abe9e8651b5
K Jaswanth <118440654+547Jaswanth@users.noreply.github.com>
2025-05-27 11:18:47 +0530

    Time: 1 ms (74.69%) | Memory: 41.1 MB (19.43%) - LeetSync

--- 3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.java
+++ 3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.java
@@ -2,8 +2,19 @@ class Solution {
     public int differenceOfSums(int n, int m) {
         int non_div_sum=0;
         int div_sum=0;
+        int i=1;
+        // for(int i=1;i<=n;i++)
+        // {
+        //     if(i%m==0){
+        //         div_sum+=i;
+        //     }
+        //     else{
+        //     non_div_sum+=i;
+        //     }
+        // }
+        // return non_div_sum - div_sum;
 
-        for(int i=1;i<=n;i++)
+        while(i<=n)
         {
             if(i%m==0){
                 div_sum+=i;
@@ -11,6 +22,7 @@ class Solution {
             else{
             non_div_sum+=i;
             }
+            i++;
         }
         return non_div_sum - div_sum;
     }
